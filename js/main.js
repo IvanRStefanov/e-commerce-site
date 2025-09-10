1a81942ca203a8071b20a38841d086529479a92f
Ivan <ivan.st3fan0v@gmail.com>
2025-09-10 16:44:37 +0300

    Add styling and slider functionality for  product slider

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 $(document).ready(function () {
-    console.log("jQuery is working!");
     $('.js-slider-gallery').each((i, slider) => {
-       console.log(slider); 
+        new Swiper(slider, {
+            autoplay: false,
+            slidesPerView: 1,
+            spaceBetween: 24,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                type: 'bullets',
+            },
+            navigation: {
+                nextEl: '.slider__next',
+                prevEl: '.slider__prev',
+            },
+            centeredSlides: true,
+        })
+    });
 
-       new Swiper(slider, {
-        autoplay: false,
-        slidesPerView: 1,
-        spaceBetween: 24,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            type: 'bullets',
-        },
-        navigation: {
-            nextEl: '.slider__next',
-            prevEl: '.slider__prev',
-        },
-        centeredSlides: true,
-       })
+    $('.js-thumbs-slider-product').each((i, slider) => {
+
+        const mainSlider = $(slider).find('.slider__clip-main')[0];
+        const thumbsSlider = $(slider).find('.slider__clip-thumbs')[0];
+        
+        const thumbsSwiper = new Swiper(thumbsSlider, {
+            slidesPerView: 'auto',
+            watchSlidesProgress: true,
+        });
+
+        const mainSwiper = new Swiper(mainSlider, {
+            slidesPerView: 1,
+            effect: "fade",
+            loop: true,
+            autoplay: false,
+            thumbs: {
+                swiper: thumbsSwiper
+            },
+        });
     });
 });
